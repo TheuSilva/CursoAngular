357cf26b51cd6d7d188b624129c92f524c312f05
Matheus <matheus.silvab98@gmail.com>
2020-08-25 16:40:33 -0300

    Integração com a API feita trazendo as fotos importando o HttpClient Module no app.module e fazendo a requisição no app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  photos = [
-    {
-      url: 'https://a-static.mlcdn.com.br/470x352/quebra-cabeca-500-pecas-paisagens-deslumbrantes-havai-toyster/casaferrari/ts2634havai/8ebda4afcb9d9abad92d2e2b569fc33c.jpg', 
-      description: 'Paisagem 1'
-    },
+  photos: Object[] = [];
+  
+  constructor(http: HttpClient){
 
-    {
-      url: 'https://s2.glbimg.com/PWzElwICb5ItVqUPSQmj6bxMkSY=/620x455/e.glbimg.com/og/ed/f/original/2014/07/29/caverna-melissani-kefalonia-grecia.jpg', 
-      description: 'Paisagem 2'
-    }
-  ];
+    http
+    .get<Object[]>('http://localhost:3000/flavio/photos')
+    .subscribe(photos => this.photos = photos);
+  }
 
 }

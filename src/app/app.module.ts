aeac750eee532705a2c1287ef87fef4429db0244
Matheus <matheus.silvab98@gmail.com>
2020-08-25 18:50:36 -0300

    Novo componente adicionado para exibição e tratamento das fotos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 import { PhotosModule } from './photos/photos.module';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoListComponent
   ],
   imports: [
     BrowserModule,
-    PhotosModule,
-    HttpClientModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]

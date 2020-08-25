c7a16da501d0cc3db606d51012bbdf595253a968
Matheus <matheus.silvab98@gmail.com>
2020-08-25 15:36:06 -0300

    Separando os módulos e importando para que haja comunicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  photos = [
+    {
+      url: 'https://a-static.mlcdn.com.br/470x352/quebra-cabeca-500-pecas-paisagens-deslumbrantes-havai-toyster/casaferrari/ts2634havai/8ebda4afcb9d9abad92d2e2b569fc33c.jpg', 
+      description: 'Paisagem 1'
+    },
+
+    {
+      url: 'https://s2.glbimg.com/PWzElwICb5ItVqUPSQmj6bxMkSY=/620x455/e.glbimg.com/og/ed/f/original/2014/07/29/caverna-melissani-kefalonia-grecia.jpg', 
+      description: 'Paisagem 2'
+    }
+  ];
+
 }
